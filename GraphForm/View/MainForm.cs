6e441b8e16ff192d8c3b74a33cf53888bb3da508
SilverGeri <angeli.gergely@gmail.com>
2014-12-10 22:04:09 +0100

    remove unused includes

--- GraphForm/View/MainForm.cs
+++ GraphForm/View/MainForm.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using GraphForm.Model;
 
 namespace GraphForm.View
