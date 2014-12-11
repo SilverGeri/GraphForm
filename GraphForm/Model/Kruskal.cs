d7dcf1c69ef6cb5c3068d08222b3538132c6cdc9
SilverGeri <angeli.gergely@gmail.com>
2014-12-11 23:06:11 +0100

    Kruskal fixed

--- GraphForm/Model/Kruskal.cs
+++ GraphForm/Model/Kruskal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace GraphForm.Model
 {
@@ -8,18 +9,25 @@ namespace GraphForm.Model
     class Kruskal : GraphAlgorythmBase
     {
         private List<Edge> _edges;
+        private List<HashSet<Node>> _sets;
+        private bool _end;
         public Kruskal(ref List<Node> graph) : base(ref graph)
         {
             _edges = new List<Edge>();
+            _sets = new List<HashSet<Node>>();
         }
 
         public override void Init()
         {
             _edges.Clear();
+            _end = false;
             foreach (var node in _graph)
             {
                 node.State = NodeState.White;
                 RaiseNodeStateChangeEvent(node.Index, node.State);
+
+                _sets.Add(new HashSet<Node>());
+                _sets[node.Index].Add(node);
 
                 foreach (var edge in node.Edges)
                 {
@@ -43,7 +51,13 @@ namespace GraphForm.Model
                 _edges.FindAll(
                     x =>
                         x.EdgeState == EdgeState.Default &&
-                        (_graph[x.StartNode].State == NodeState.White || _graph[x.EndNode].State == NodeState.White));
+                        (_sets.FindIndex(y=> y.Contains(_graph[x.StartNode])) != _sets.FindIndex(y=> y.Contains(_graph[x.EndNode]))));
+
+            if (possibleEdges.Count == 0)
+            {
+                _end = true;
+                return;
+            }
             var nextEdge = 0;
             for (var i = 1; i < possibleEdges.Count; ++i)
             {
@@ -53,18 +67,34 @@ namespace GraphForm.Model
                 }
             }
 
+            int startSet = _sets.FindIndex(y => y.Contains(_graph[possibleEdges[nextEdge].StartNode]));
+            int endSet = _sets.FindIndex(y => y.Contains(_graph[possibleEdges[nextEdge].EndNode]));
+
+            if (startSet < endSet)
+            {
+                foreach (var node in _sets[endSet])
+                {
+                    _sets[startSet].Add(node);
+                }
+                _sets[endSet].Clear();
+            }
+            else
+            {
+                foreach (var node in _sets[startSet])
+                {
+                    _sets[endSet].Add(node);
+                }
+                _sets[startSet].Clear();
+            }
+
             possibleEdges[nextEdge].EdgeState = EdgeState.Selected;
             RaiseEdgeStateChangeEvent(possibleEdges[nextEdge].Id, possibleEdges[nextEdge].EdgeState);
-
-            _graph[possibleEdges[nextEdge].StartNode].State = NodeState.Black;
-            RaiseNodeStateChangeEvent(possibleEdges[nextEdge].StartNode, _graph[possibleEdges[nextEdge].StartNode].State);
-            _graph[possibleEdges[nextEdge].EndNode].State = NodeState.Black;
-            RaiseNodeStateChangeEvent(possibleEdges[nextEdge].EndNode, _graph[possibleEdges[nextEdge].EndNode].State);
         }
 
         public override bool End()
         {
-            return _graph.All(x => x.State == NodeState.Black);
+            //all nodes are in the same set
+            return _end;
         }
     }
 }
